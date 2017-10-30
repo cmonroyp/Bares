042e37102810754c268d93d26a33113e31e985db
PC-CMONROY\Cmonroyp <carlosmonroy@outlook.com>
2017-10-29 20:39:59 -0500

    casa

--- src/pages/modalreservas/modalreservas.ts
+++ src/pages/modalreservas/modalreservas.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the ModalreservasPage page.
@@ -15,22 +15,57 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class ModalreservasPage {
 
+  reservasCheck:boolean;
+
   list:Array<any>= [
-    {mesa:'mesa1',estado:[{state:'libre'}]},
-    {mesa:'mesa2',estado:[{state:'reservada'}]},
-    {mesa:'mesa3',estado:[{state:'reservada'}]},
-    {mesa:'mesa4',estado:[{state:'libre'}]},
-    {mesa:'mesa5',estado:[{state:'reservada'}]},
+    {mesa:'mesa1',estado:[{state:'libre'}],checked:'false'},
+    {mesa:'mesa2',estado:[{state:'reservada'}],checked:'true'},
+    {mesa:'mesa3',estado:[{state:'reservada'}],checked:'true'},
+    {mesa:'mesa4',estado:[{state:'libre'}],checked:'false'},
+    {mesa:'mesa5',estado:[{state:'reservada'}],checked:'true'},
   ];
 
   state:boolean = true;
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              private viewCtrl: ViewController) {
-              console.log(this.list)
+              private viewCtrl: ViewController,
+              private alertCtrl: AlertController) {
+              //console.log(this.list)
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ModalreservasPage');
+    for (var state of this.list) {
+      console.log(state);
+      if (state['checked'] === 'true') {
+        this.reservasCheck = true;
+        console.log('entro',this.reservasCheck);
+      }
+    }
+  }
+
+  checkReservas(check:any){
+    let confirm = this.alertCtrl.create({
+      title: 'Reservas?',
+      message: 'Desea Reservar esta Mesa?.',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Si',
+          handler: () => {
+            console.log('Agree clicked');
+            this. reservasCheck = check;
+            console.log(this.reservasCheck)
+          }
+        }
+      ]
+    });
+    confirm.present();
+  
+    
   }
 
   closeModal() {
